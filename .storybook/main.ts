6a8f6dd5cf0feb721718cced4d9597b00406f34d
mBukator <m.bukator03@gmail.com>
2024-11-28 15:43:14 +0100

    [Chore]: Setup Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,38 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const config: StorybookConfig = {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    typescript: {
+        reactDocgen: 'react-docgen-typescript'
+    },
+    addons: [
+        "@storybook/addon-onboarding",
+        "@storybook/addon-essentials",
+        "@chromatic-com/storybook",
+        "@storybook/addon-interactions",
+        "@storybook/addon-docs"
+    ],
+    framework: {
+        name: "@storybook/react-vite",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+    viteFinal: async (config) => {
+        config.plugins?.push(
+            tsconfigPaths({
+                projects: [
+                    path.resolve(
+                        path.dirname(__dirname),
+                        "tsconfig.app.json"
+                    ),
+                ],
+            })
+        );
+        return config;
+    },
+};
+export default config;
