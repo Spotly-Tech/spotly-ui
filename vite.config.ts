52d48088ce5d8f487baa1a68164387db20d50a51
mBukator <m.bukator03@gmail.com>
2024-12-05 03:47:17 +0100

    [Chore]: Add jsdom test environment configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,4 +7,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    test: {
+        environment: "jsdom",
+        globals: true,
+    },
 });
