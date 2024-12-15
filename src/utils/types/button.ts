b9519092996b90c4284e1af92d9aa3a0f372daaa
mBukator <m.bukator03@gmail.com>
2024-12-15 22:26:31 +0100

    [Feat(types)]: Add file for types and interfaces of Button

---
+++ src/utils/types/button.ts
@@ -0,0 +1,40 @@
+import { ButtonHTMLAttributes } from "react";
+
+import { MotionProps } from "framer-motion";
+
+export type MotionButtonProps = MotionProps & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export interface IButtonBaseProps extends MotionButtonProps {
+    variant?: "ghost" | "solid" | "outlined";
+    size?: "small" | "medium" | "large" | "fullwidth";
+    color?: "default" | "primary" | "secondary" | "danger";
+    shape?: "square" | "circle";
+    isLoading?: boolean;
+    loadingText?: string;
+    children?: React.ReactNode;
+    disabled?: boolean;
+    animated?: boolean;
+    animation?: MotionProps;
+}
+
+export interface IButtonDefaultProps extends IButtonBaseProps {
+    btnType?: "button";
+    icon?: never;
+    iconPosition?: never;
+}
+
+export interface IIconButtonProps extends IButtonBaseProps {
+    btnType: "icon";
+    icon: React.ReactNode;
+    children?: never;
+    iconPosition?: never;
+}
+
+export interface IWithIconButtonProps extends IButtonBaseProps {
+    btnType: "with-icon";
+    icon: React.ReactNode;
+    iconPosition: "start" | "end";
+    children: React.ReactNode;
+}
+
+export type ButtonProps = IButtonDefaultProps | IIconButtonProps | IWithIconButtonProps;
