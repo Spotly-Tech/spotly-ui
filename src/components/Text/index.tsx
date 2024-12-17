bdfb61f69b1849acf5cf935ea980788f719b6178
mBukator <m.bukator03@gmail.com>
2024-12-17 23:35:31 +0100

    [Refactor]: Enable ref forwarding in Text component
    - Refactored Text component to use forwardRef
    
    - Added @ts-expect-error to address type inconsistencies
    
    - Improved type handling with PolymorphicRef and refined generics for 'as' prop

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -5,36 +5,43 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from "react";
+import React, { forwardRef } from "react";
 
-import { PolymorphicComponentProps } from "@/utils/types";
-import { TextProps } from "@/utils/types/text";
+import { PolymorphicRef } from "@/utils/types";
+import { TextComponent, TextProps } from "@/utils/types/text";
 
-// If as prop isn't passed to Text component by default it would be rendered as <span>
-export const Text = <C extends React.ElementType = "span">({
-    as,
-    color = "default",
-    emphasis = "default",
-    size = "base",
-    weight = "normal",
-    align = "left",
-    italic = false,
-    underline = false,
-    children,
-    ...restProps
-}: PolymorphicComponentProps<C, TextProps>) => {
-    const Component = as || "span";
+// @ts-expect-error - unexpected typing errors
+export const Text: TextComponent = forwardRef(
+    // @ts-expect-error - unexpected typing errors
+    <C extends React.ElementType = "span">(
+        {
+            as,
+            color = "default",
+            emphasis = "default",
+            size = "base",
+            weight = "normal",
+            align = "left",
+            italic = false,
+            underline = false,
+            children,
+            ...restProps
+        }: TextProps<C>,
+        ref?: PolymorphicRef<C>
+    ) => {
+        const Component = as || "span";
 
-    const baseClass = `SpotlyUI-text SpotlyUI-text--color-${color} SpotlyUI-text--size-${size} SpotlyUI-text--weight-${weight} SpotlyUI-text--align-${align} SpotlyUI-text--emphasis-${emphasis}`;
-    const italicClass = italic ? "SpotlyUI-text--italic" : "";
-    const underlineClass = underline ? "SpotlyUI-text--underline" : "";
+        const baseClass = `SpotlyUI-text SpotlyUI-text--color-${color} SpotlyUI-text--size-${size} SpotlyUI-text--weight-${weight} SpotlyUI-text--align-${align} SpotlyUI-text--emphasis-${emphasis}`;
+        const italicClass = italic ? "SpotlyUI-text--italic" : "";
+        const underlineClass = underline ? "SpotlyUI-text--underline" : "";
 
-    return (
-        <Component
-            className={`${baseClass} ${italicClass} ${underlineClass}`}
-            {...restProps}
-        >
-            {children}
-        </Component>
-    );
-};
+        return (
+            <Component
+                ref={ref}
+                className={`${baseClass} ${italicClass} ${underlineClass}`}
+                {...restProps}
+            >
+                {children}
+            </Component>
+        );
+    }
+);
