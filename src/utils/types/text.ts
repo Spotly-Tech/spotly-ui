e1077bebf48f1ba26bb5abe8c424a1263dbfd458
mBukator <m.bukator03@gmail.com>
2024-12-19 00:40:59 +0100

    [Feat(Text)]: Add 'default-gradient' option to color prop

--- src/utils/types/text.ts
+++ src/utils/types/text.ts
@@ -3,7 +3,7 @@ import { PolymorphicComponentPropsWithRef } from "./index";
 export type TextProps<C extends React.ElementType> = PolymorphicComponentPropsWithRef<
     C,
     {
-        color?: "default" | "primary" | "secondary" | "danger" | "warning" | "info";
+        color?: "default" | "default-gradient" | "primary" | "secondary" | "danger" | "warning" | "info";
         emphasis?: "default" | "low";
         size?: "2xs" | "xs" | "sm" | "base" | "lg" | "xl" | "2xl" | "3xl";
         weight?: "thin" | "normal" | "medium" | "semibold" | "bold" | "black";
