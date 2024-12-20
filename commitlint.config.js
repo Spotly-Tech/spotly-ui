8180000f4308921a44fab29cdc4a52f573cb9bec
mBukator <m.bukator03@gmail.com>
2024-12-20 15:37:14 +0100

    chore: update commitlint config
    - Remove custom setting from commitlint config to stick with conventional commits
    
    - Remove custom parser for commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,28 +1,3 @@
 export default {
     extends: ["@commitlint/config-conventional"],
-    rules: {
-        "header-case": [2, "always", "sentence-case"],
-        "type-enum": [
-            2,
-            "always",
-            [
-                "Build",
-                "Chore",
-                "CI",
-                "Docs",
-                "Feat",
-                "Fix",
-                "Perf",
-                "Refactor",
-                "Revert",
-                "Style",
-                "Test",
-            ],
-        ],
-        "type-case": [2, "always", "sentence-case"],
-        "scope-case": [2, "always", ["sentence-case"]],
-        "subject-case": [2, "always", ["sentence-case"]],
-        "body-case": [2, "always", "sentence-case"],
-    },
-    parserPreset: "./parser-preset.js",
 };
--- parser-preset.js
+++
@@ -1,6 +0,0 @@
-export default {
-    parserOpts: {
-        headerPattern: /^\[([A-Za-z0-9-]+)(?:\((\w*)\))?\]: (.+)$/,
-        headerCorrespondence: ["type", "scope", "subject"],
-    },
-};
