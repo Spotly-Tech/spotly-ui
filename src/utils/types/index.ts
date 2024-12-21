15db707f29ac0c59438b71d5b48f49575103ecca
mBukator <m.bukator03@gmail.com>
2024-12-21 19:28:24 +0100

    feat: add shared text props interface

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Spotly Tech
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 export type PolymorphicComponentProps<
     C extends React.ElementType,
--- src/utils/types/text.ts
+++ src/utils/types/text.ts
@@ -1,12 +1,31 @@
+/**
+ * Copyright (c) Spotly Tech
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { PolymorphicComponentPropsWithRef } from "./index";
 
+/* SHARED TEXT PROPS */
+export interface SharedTextProps {
+    color?:
+        | "default"
+        | "default-gradient"
+        | "primary"
+        | "secondary"
+        | "danger"
+        | "warning"
+        | "info";
+    size?: "2xs" | "xs" | "sm" | "base" | "lg" | "xl" | "2xl" | "3xl";
+    weight?: "thin" | "normal" | "medium" | "semibold" | "bold" | "black";
+}
+
+/* TEXT PROPS */
 export type TextProps<C extends React.ElementType> = PolymorphicComponentPropsWithRef<
     C,
-    {
-        color?: "default" | "default-gradient" | "primary" | "secondary" | "danger" | "warning" | "info";
+    SharedTextProps & {
         emphasis?: "default" | "high" | "medium" | "low" | "low-bold";
-        size?: "2xs" | "xs" | "sm" | "base" | "lg" | "xl" | "2xl" | "3xl";
-        weight?: "thin" | "normal" | "medium" | "semibold" | "bold" | "black";
         align?: "left" | "center" | "right";
         italic?: boolean;
         underline?: boolean;
@@ -14,4 +33,12 @@ export type TextProps<C extends React.ElementType> = PolymorphicComponentPropsWi
 >;
 export type TextComponent = <C extends React.ElementType = "span">(
     props: TextProps<C>
-  ) => React.ReactElement | null;
+) => React.ReactElement | null;
+
+/* FLIP TEXT PROPS */
+export interface FlipTextProps extends SharedTextProps {
+    children: string;
+    duration?: number;
+    delay?: number;
+    isLooped?: boolean;
+}
