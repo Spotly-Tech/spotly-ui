b9e71daca061df5404ecf38273e786a19f6192de
mBukator <m.bukator03@gmail.com>
2024-12-25 23:55:00 +0100

    feat(vite): enhance configuration with TypeScript support and build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,37 @@
 /// <reference types="vitest" />
 
+import path from "path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths(), dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" })],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/components/index.tsx"),
+            name: "SpotlyUI",
+            fileName: "spotly-ui",
+            formats: ["es", "umd"],
+        },
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
+    },
     test: {
         environment: "jsdom",
         globals: true,
