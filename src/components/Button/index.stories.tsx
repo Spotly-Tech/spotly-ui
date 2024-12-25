e9b561f67281d3b3156125649141596a38df9a47
mBukator <m.bukator03@gmail.com>
2024-12-25 20:27:48 +0100

    feat(Button): update stories with detailed prop tables

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -6,6 +6,7 @@
  */
 
 import type { Meta, StoryObj } from "@storybook/react";
+
 import { Button } from "./index";
 
 const meta: Meta<typeof Button> = {
@@ -22,49 +23,84 @@ const meta: Meta<typeof Button> = {
             control: { type: "inline-radio" },
             description:
                 "Specifies the button's visual style: `ghost` for text-only buttons, `filled` for solid buttons, `outlined` for bordered buttons.",
+            table: {
+                type: { summary: "string" },
+                defaultValue: { summary: "solid" },
+            },
         },
         size: {
             options: ["small", "medium", "large", "fullwidth"],
             control: { type: "inline-radio" },
             description:
-                "Defines the button's size. `fullwidth` makes the button span the full width of its container.	",
+                "Defines the button's size. `fullwidth` makes the button span the full width of its container.",
+            table: {
+                type: { summary: "string" },
+                defaultValue: { summary: "medium" },
+            },
         },
         color: {
             options: ["default", "primary", "secondary", "danger"],
             control: { type: "inline-radio" },
             description: "Specifies the color scheme for the button.",
+            table: {
+                type: { summary: "string" },
+                defaultValue: { summary: "default" },
+            },
         },
         btnType: {
             options: ["button", "icon", "with-icon"],
             control: { type: "inline-radio" },
             description:
                 "Specifies the button type: `button` for standard buttons, `icon` for icon-only buttons, `with-icon` for buttons with text and an icon.",
+            table: {
+                type: { summary: "string" },
+                defaultValue: { summary: "button" },
+            },
         },
         shape: {
             options: ["square", "circle"],
             control: { type: "inline-radio" },
             description:
                 "Sets the button's shape. `circle` is only applicable when btnType is `icon`.",
+            table: {
+                type: { summary: "string" },
+                defaultValue: { summary: "square" },
+            },
         },
         iconPosition: {
             options: ["start", "end"],
             control: { type: "inline-radio" },
             description:
                 "Determines the position of the icon relative to the text. Only applies if btnType is `with-icon`.",
+            table: {
+                type: { summary: "string" },
+                defaultValue: { summary: "start" },
+            },
         },
         isLoading: {
             control: { type: "boolean" },
             description:
                 "Indicates if the button is in a loading state. Displays `loadingText` instead of `children` when `true`",
+            table: {
+                type: { summary: "boolean" },
+                defaultValue: { summary: "false" },
+            },
         },
         loadingText: {
             control: { type: "text" },
-            description:
-                "Text to display when the button is in a loading state.",
+            description: "Text to display when the button is in a loading state.",
+            table: {
+                type: { summary: "string" },
+                defaultValue: { summary: "Loading..." },
+            },
         },
         disabled: {
             control: { type: "boolean" },
             description: "Disables the button when set to `true`.",
+            table: {
+                type: { summary: "boolean" },
+                defaultValue: { summary: "false" },
+            },
         },
         children: {
             control: { type: "text" },
@@ -78,17 +114,29 @@ const meta: Meta<typeof Button> = {
             control: { type: "object" },
             description:
                 "Defines the icon to display. Applicable only when btnType is `icon` or `with-icon`.",
+            table: {
+                type: { summary: "ReactNode" },
+                defaultValue: { summary: "" },
+            },
         },
         animated: {
             options: [true, false],
             control: { type: "inline-radio" },
             description:
                 "Enables or disables animation on the button. When set to `true`, default hover and tap animations are applied.",
+            table: {
+                type: { summary: "boolean" },
+                defaultValue: { summary: "false" },
+            },
         },
         animation: {
             control: { type: "object" },
             description:
                 "Custom animation configuration using Framer Motion props. Overrides the default animations if `animated` is set to `true`.",
+            table: {
+                type: { summary: "object" },
+                defaultValue: { summary: "{}" },
+            },
         },
     },
 };
@@ -114,11 +162,9 @@ export const Variants: Story = {
 
             source: {
                 code: `
-<div>
-    <Button variant="solid">Solid</Button>
-    <Button variant="ghost">Ghost</Button>
-    <Button variant="outlined">Outlined</Button>
-</div>
+<Button variant="solid">Solid</Button>
+<Button variant="ghost">Ghost</Button>
+<Button variant="outlined">Outlined</Button>
                 `,
             },
         },
@@ -134,7 +180,8 @@ export const Sizes: Story = {
                 gap: "10px",
                 alignItems: "baseline",
                 width: "100%",
-            }}>
+            }}
+        >
             <Button size="small">Small</Button>
             <Button size="medium">Medium</Button>
             <Button size="large">Large</Button>
@@ -148,11 +195,9 @@ export const Sizes: Story = {
             },
             source: {
                 code: `
-<div>
-    <Button size="small">Small</Button>
-    <Button size="medium">Medium</Button>
-    <Button size="large">Large</Button>
-</div>
+<Button size="small">Small</Button>
+<Button size="medium">Medium</Button>
+<Button size="large">Large</Button>
                 `,
             },
         },
@@ -174,13 +219,11 @@ export const Colors: Story = {
             },
             source: {
                 code: `
-<div>
-    <Button color="default">Default</Button>
-    <Button color="primary">Primary</Button>
-    <Button color="secondary">Secondary</Button>
-    <Button color="danger">Danger</Button>
-</div>
-                `.trim(),
+<Button color="default">Default</Button>
+<Button color="primary">Primary</Button>
+<Button color="secondary">Secondary</Button>
+<Button color="danger">Danger</Button>
+                `,
             },
         },
     },
@@ -196,7 +239,8 @@ export const Animations: Story = {
                 animation={{
                     whileHover: { scale: 1.1, rotate: 5 },
                     whileTap: { scale: 0.95, rotate: -5 },
-                }}>
+                }}
+            >
                 Custom Animation
             </Button>
             <Button animated={false}>No Animation</Button>
@@ -209,18 +253,16 @@ export const Animations: Story = {
             },
             source: {
                 code: `
-<div>
-    <Button animated>Default Animation</Button>
-    <Button
-        animated
-        animation={{
-            whileHover: { scale: 1.1, rotate: 5 },
-            whileTap: { scale: 0.95, rotate: -5 },
-        }}>
-        Custom Animation
-    </Button>
-    <Button animated={false}>No Animation</Button>
-</div>
+<Button animated>Default Animation</Button>
+<Button
+    animated
+    animation={{
+        whileHover: { scale: 1.1, rotate: 5 },
+        whileTap: { scale: 0.95, rotate: -5 },
+    }}>
+    Custom Animation
+</Button>
+<Button animated={false}>No Animation</Button>
                 `,
             },
         },
@@ -243,15 +285,11 @@ export const States: Story = {
             },
             source: {
                 code: `
-<div>
-    <Button>Normal</Button>
-    <Button isLoading>Loading</Button>
-    <Button disabled>Disabled</Button>
-</div>
+<Button>Normal</Button>
+<Button isLoading>Loading</Button>
+<Button disabled>Disabled</Button>
                 `,
             },
         },
     },
 };
-
-
