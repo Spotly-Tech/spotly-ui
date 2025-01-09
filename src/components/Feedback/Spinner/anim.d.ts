5152ac1e5302bdff6485cfd269e53cd5307ab048
mBukator <m.bukator03@gmail.com>
2025-01-09 02:34:12 +0100

    feat(spinner): add animation logic for spinner component

---
+++ src/components/Feedback/Spinner/anim.d.ts
@@ -0,0 +1,17 @@
+export declare const spinner: (
+    spinSpeed: number,
+) => {
+    unspecified: {
+        rotate: number[];
+        transition: {
+            repeat: number;
+            duration: number;
+            ease: string;
+        };
+    };
+    specified: {
+        transition: {
+            duration: number;
+        };
+    };
+};
