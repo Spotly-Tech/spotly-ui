960929e32f266bd77d80ee7a704b5a0e4a8fe403
mBukator <m.bukator03@gmail.com>
2025-01-29 23:49:52 +0100

    feat(Row): add Row component

---
+++ src/components/Layout/Row/index.tsx
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Spotly Tech
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { forwardRef } from "react";
+
+import { RowProps } from "@/utils/types";
+
+import "@/components/shared/SharedLayout.css";
+import "./Row.css";
+
+export const Row = forwardRef<HTMLDivElement, RowProps>(
+    (
+        {
+            as,
+            children,
+            align = "start",
+            justify = "start",
+            spacing = "none",
+            wrap = true,
+            className,
+            ...restProps
+        },
+        ref
+    ) => {
+        const baseClass = `SpotlyUI-row SpotlyUI-row--align-${align} SpotlyUI-row--justify-${justify} SpotlyUI-row--spacing-${spacing} ${wrap ? "SpotlyUI-row--wrap" : "SpotlyUI-row--nowrap"} ${className || ""}`;
+        const Component = as || "div";
+        return (
+            <Component ref={ref} className={baseClass} {...restProps}>
+                {children}
+            </Component>
+        );
+    }
+);
