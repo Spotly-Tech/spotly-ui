e6ea13ea2cd84bf9055fc8ee66e321fabac74cbd
mBukator <m.bukator03@gmail.com>
2025-02-02 02:35:23 +0100

    chore(Typography): add license comment to index file

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,2 +1,9 @@
+/**
+ * Copyright (c) Spotly Tech
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 export { FlipText } from "./FlipText";
 export { Text } from "./Text";
