16dcedf9aa6e9c2578c09a55676b0cb975bfe80b
mBukator <m.bukator03@gmail.com>
2025-02-04 22:23:39 +0100

    chore(eslint): extend global variables to include Node.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from "typescript-eslint";
 export default [
     { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
     { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-    { languageOptions: { globals: globals.browser } },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     ...fixupConfigRules(pluginReactConfig),
