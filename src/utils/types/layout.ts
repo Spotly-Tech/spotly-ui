13fd9d9f2804f8c92eabe4ea76700d9a212e391f
mBukator <m.bukator03@gmail.com>
2025-02-05 00:20:09 +0100

    feat(Layout): add types for Col component props

--- src/utils/types/layout.ts
+++ src/utils/types/layout.ts
@@ -5,26 +5,52 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import { ComponentProps } from "react";
+
 import { PolymorphicComponentPropsWithRef } from "./polymorphic";
 
 export type BoxProps<C extends React.ElementType> = PolymorphicComponentPropsWithRef<C>;
 
+/* Box */
 export type BoxComponent = <C extends React.ElementType = "div">(
     props: BoxProps<C>
 ) => React.ReactElement | null;
 
+/* Shared */
 type LayoutSharedProps = {
     align?: "start" | "center" | "end" | "stretch" | "baseline";
     justify?: "start" | "center" | "end" | "between" | "around" | "evenly";
     spacing?: "none" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl";
 };
 
+/* Stack */
 export type StackProps = BoxProps<"div"> &
     LayoutSharedProps & {
         direction?: "row" | "row-reverse" | "column" | "column-reverse";
     };
 
+/* Row */
 export type RowProps<C extends React.ElementType = "div"> = BoxProps<C> &
     LayoutSharedProps & {
         wrap?: boolean;
+    };
+
+/* Column */
+type ColSpan = number | "auto";
+type ColBreakpoint = {
+    span?: ColSpan;
+    offset?: number;
+    order?: number;
+};
+export type ColProps = ComponentProps<"div"> &
+    LayoutSharedProps & {
+        span?: ColSpan;
+        offset?: number;
+        order?: number;
+        xs?: ColSpan | ColBreakpoint;
+        sm?: ColSpan | ColBreakpoint;
+        md?: ColSpan | ColBreakpoint;
+        lg?: ColSpan | ColBreakpoint;
+        xl?: ColSpan | ColBreakpoint;
+        xxl?: ColSpan | ColBreakpoint;
     };
