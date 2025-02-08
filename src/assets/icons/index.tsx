5d98c4f63e9d579a7152eccbf4b517dfe4bec532
mBukator <m.bukator03@gmail.com>
2025-02-08 17:38:00 +0100

    feat(icons): add index file to export icon components

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,4 @@
+export { CheckCircle } from "./CheckCircle";
+export { ErrorCircle } from "./ErrorCircle";
+export { InfoCircle } from "./InfoCircle";
+export { WarningCircle } from "./WarningCircle";
