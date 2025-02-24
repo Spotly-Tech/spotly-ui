45088a32eb1d40ec2d05442d2b52ff42e55de29f
mBukator <m.bukator03@gmail.com>
2025-02-24 13:01:30 +0100

    feat(Feedback): add export of Ripple component

--- src/components/Feedback/index.tsx
+++ src/components/Feedback/index.tsx
@@ -5,5 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export { Spinner } from "./Spinner";
 export { Alert } from "./Alert";
+export { Ripple } from "./Ripple";
+export { Spinner } from "./Spinner";
