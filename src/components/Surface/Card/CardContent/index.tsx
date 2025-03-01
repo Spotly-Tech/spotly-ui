157e4a3f17782d3f273f8d5f9afa822b74849ee9
mBukator <m.bukator03@gmail.com>
2025-03-01 19:32:26 +0100

    feat(Card): add CardContent component

---
+++ src/components/Surface/Card/CardContent/index.tsx
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Spotly Tech
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { forwardRef, Ref } from "react";
+
+import { CardContentComponent, CardContentProps } from "@/utils/types/surface";
+
+import "./CardContent.css";
+
+export const CardContent: CardContentComponent = forwardRef(
+    (
+        { children, className = "", ...restProps }: CardContentProps,
+        ref: Ref<HTMLDivElement>
+    ) => {
+        const baseClass = `SpotlyUI-card-content`;
+        return (
+            <div ref={ref} className={`${baseClass} ${className}`} {...restProps}>
+                {children}
+            </div>
+        );
+    }
+);
