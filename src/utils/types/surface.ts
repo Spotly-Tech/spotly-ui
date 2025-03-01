e0660737ce5075247fd3b02fd22b664db4fc8677
mBukator <m.bukator03@gmail.com>
2025-03-01 19:27:46 +0100

    feat(Surface): add shared and CardProps types

---
+++ src/utils/types/surface.ts
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) Spotly Tech
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { ComponentProps, ForwardRefExoticComponent, RefAttributes } from "react";
+
+import { MotionProps } from "motion/react";
+
+/*======== SHARED SURFACE PROPS ========*/
+export type SharedSurfaceProps = ComponentProps<"div"> & {
+    width?: string;
+    height?: string;
+    borderRadius?: string;
+    className?: string;
+    children?: React.ReactNode;
+    elevation?: 0 | 1 | 2 | 3 | 4 | 5;
+    variant?: "solid" | "solid-outlined";
+};
+
+/*======== CARD ========*/
+export type CardProps = SharedSurfaceProps &
+    MotionProps & {
+        hoverScale?: number;
+        hoverDuration?: number;
+        hoverEase?:
+            | "anticipate"
+            | "backInOut"
+            | "backIn"
+            | "backOut"
+            | "circInOut"
+            | "circIn"
+            | "circOut"
+            | "easeInOut"
+            | "easeIn"
+            | "easeOut"
+            | "linear";
+        hoverable?: boolean;
+    };
+export type CardComponent = ForwardRefExoticComponent<
+    CardProps & RefAttributes<HTMLDivElement>
+>;
+
