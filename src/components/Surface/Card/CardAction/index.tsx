bb5af253804abe2ad06de13414eb18618caa786a
mBukator <m.bukator03@gmail.com>
2025-03-03 00:43:32 +0100

    feat(Card): add CardAction component

---
+++ src/components/Surface/Card/CardAction/index.tsx
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) Spotly Tech
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { forwardRef } from "react";
+
+import { CardActionComponent, CardActionProps } from "@/utils/types/surface";
+
+import "./CardAction.css";
+
+export const CardAction: CardActionComponent = forwardRef<
+    HTMLDivElement,
+    CardActionProps
+>(({ children, className, style, ...restProps }, ref) => {
+    const baseClass = `SpotlyUI-card-action`;
+    return (
+        <div
+            ref={ref}
+            className={`${baseClass} ${className}`}
+            style={{
+                ...style,
+            }}
+            {...restProps}
+        >
+            {children}
+        </div>
+    );
+});
